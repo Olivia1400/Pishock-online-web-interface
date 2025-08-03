7b8d3166174fb66de64b50281beb7ab78305eb84
Olivia1400 <livvyanderson2006@gmail.com>
2025-08-03 16:22:55 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,9 +1,9 @@
 from pishock import PiShockAPI
 
 
-username = "OliviaAnders"   # from pishock.com
-api_key = "b3375484-fb05-491c-817b-a9a504ced77e"    # https://pishock.com/#/account
-sharecode = "1C655979510"  # https://pishock.com/#/control (share button)
+username = ""   # from pishock.com
+api_key = ""    # Enter API key here https://pishock.com/#/account
+sharecode = ""  # https://pishock.com/#/control (share button)
 
 def main(intensity, duration):
     dur = duration
